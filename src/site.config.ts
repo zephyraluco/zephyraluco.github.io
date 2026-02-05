540bf3529a13df4b6e22b053fa9514f496a6c1c3
zephyraluco <zephyraluco@gmail.com>
2026-02-05 23:50:36 +0800

    docs: tspi烧录教程

--- src/site.config.ts
+++ src/site.config.ts
@@ -159,7 +159,7 @@ export const integ: IntegrationUserConfig = {
   },
   // Comment system
   waline: {
-    enable: true,
+    enable: false,
     // Server service link
     server: 'https://astro-theme-pure-waline.arthals.ink/',
     // Show meta info for comments
